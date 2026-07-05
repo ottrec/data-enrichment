af9b6a8da969e14f9320373f6dca31c23d13d96b
Patrick Gaskin <patrick@pgaskin.net>
2026-07-05 02:51:41 -0400

    wip

---
+++ cmd/dump-special/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"iter"
+
+	"github.com/ottrec/website/pkg/ottrecdata"
+	"github.com/ottrec/website/pkg/ottrecidx"
+)
+
+func main() {
+	ctx := context.Background()
+
+	var err error
+	for ver, idx := range versions(ctx, "/tmp/ottrec-data.db")(&err) {
+		fmt.Println(ver.ID, ver.Updated, ver.Revision, idx)
+	}
+	if err != nil {
+		panic(err)
+	}
+}
+
+func versions(ctx context.Context, cachePath string) func(*error) iter.Seq2[ottrecdata.DataVersion, *ottrecidx.Index] {
+	return func(err *error) iter.Seq2[ottrecdata.DataVersion, *ottrecidx.Index] {
+		return func(yield func(ottrecdata.DataVersion, *ottrecidx.Index) bool) {
+			*err = func() error {
+				cache, err := ottrecdata.OpenCacheReadOnly(cachePath)
+				if err != nil {
+					return fmt.Errorf("open cache %q: %w", cachePath, err)
+				}
+				defer cache.Close()
+
+				for ver := range cache.DataVersions(ctx)(&err) {
+					var pbh string
+					for hash, fmt := range cache.DataFormats(ctx, ver.ID)(&err) {
+						if fmt == "pb" {
+							pbh = hash
+							break
+						}
+					}
+					if err != nil {
+						return fmt.Errorf("list %s formats: %w", ver.ID, err)
+					}
+					if pbh == "" {
+						return fmt.Errorf("read %s: missing pb", ver.ID)
+					}
+
+					var pb []byte
+					cache.ReadBlob(ctx, pbh, false, func(r io.Reader, i int64) (err error) {
+						pb, err = io.ReadAll(r)
+						return
+					})
+					if err != nil {
+						return fmt.Errorf("read %s: %w", ver.ID, err)
+					}
+
+					idx, err := new(ottrecidx.Indexer).Load(pb)
+					if err != nil {
+						return fmt.Errorf("load %s: %w", ver.ID, err)
+					}
+
+					if !yield(ver, idx) {
+						break
+					}
+				}
+				if err != nil {
+					return fmt.Errorf("list versions: %w", err)
+				}
+				return nil
+			}()
+		}
+	}
+}
---
+++ go.mod
@@ -0,0 +1,19 @@
+module github.com/ottrec/data-enrichment
+
+go 1.26.3
+
+require github.com/ottrec/website v0.0.0-20260704205528-cb10e228699c
+
+require (
+	github.com/kelindar/bitmap v1.5.5 // indirect
+	github.com/kelindar/simd v1.2.0 // indirect
+	github.com/klauspost/compress v1.18.6 // indirect
+	github.com/klauspost/cpuid/v2 v2.3.0 // indirect
+	github.com/ncruces/go-sqlite3 v0.35.0 // indirect
+	github.com/ncruces/go-sqlite3-wasm/v3 v3.1.35302 // indirect
+	github.com/ncruces/julianday v1.0.0 // indirect
+	github.com/ottrec/scraper v0.0.0-20260617053659-f8e60d2d1db7 // indirect
+	golang.org/x/image v0.42.0 // indirect
+	golang.org/x/sys v0.46.0 // indirect
+	google.golang.org/protobuf v1.36.11 // indirect
+)
